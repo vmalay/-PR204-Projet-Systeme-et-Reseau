640358a386382255481100b5bf0a24758ba0a720
Valerian Malay <valmalay@gmail.com>
2018-11-26 11:12:24 +0100

    Add files via upload

---
+++ PROJET_SYST/Phase2/common.c
@@ -0,0 +1,255 @@
+#include "common_impl.h"
+
+int creer_socket(int prop, int *port_num)
+{
+  int sockfd = 0;
+  struct sockaddr_in serv_addr;
+
+  /* fonction de creation et d'attachement d'une nouvelle socket */
+  sockfd = do_socket(AF_INET, prop, IPPROTO_TCP);  //Socket initialisation
+  serv_addr = init_serv_addr("0", serv_addr); //Initialisation of server information
+  do_bind(sockfd, serv_addr);  //We Bind on the port specified.
+
+  /* renvoie le numero de descripteur et modifie le parametre port_num */
+  socklen_t len = sizeof(serv_addr);
+  getsockname(sockfd,(struct sockaddr *)&serv_addr,&len);
+  *port_num = ntohs(serv_addr.sin_port);
+
+  printf("Creer_socket: succes.\n");
+
+  return sockfd;
+
+}
+
+int creer_socket_connect(char *addr,char *port){
+  struct addrinfo hints,*res,*p;
+  int status,sock;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET; 
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+  if ((status = getaddrinfo(addr, port, &hints, &res)) != 0)
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+
+  for(p = res; p != NULL; p = p->ai_next) {
+
+    //get the socket
+    sock = do_socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+
+    //connect to remote socket
+    if (connect(sock, p->ai_addr, p->ai_addrlen)!=0)
+      perror("connect dsmwrap");
+
+    }
+
+  freeaddrinfo(res);
+  return sock;
+}
+
+int do_socket(int domain, int type, int protocol){
+
+  int sockfd;
+  int yes = 1;
+
+  //create the socket
+  sockfd = socket(domain, type, protocol);
+
+  //check for socket validity
+  if (sockfd < 0)
+  perror("ERROR opening socket");
+
+  //Reusing already in use adresses
+  if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int))==-1)
+  perror("Error socket option");
+
+  return sockfd;
+}
+
+struct sockaddr_in init_serv_addr(const char* port,struct sockaddr_in serv_addr){
+
+  memset(&serv_addr,0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(atoi(port));
+
+  return serv_addr;
+
+}
+
+void do_bind(int sockfd, struct sockaddr_in serv_addr){
+  if ( bind( sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1)
+    perror ("bind");
+}
+
+void do_listen(int sockfd){
+  if (listen(sockfd, SOMAXCONN) == -1 ) {
+    perror( "listen: ");
+  }
+}
+
+
+int do_accept(int sockfd, struct sockaddr_in *client_addr){
+  struct sockaddr_in client_addr2 = *client_addr;
+  int client_sock;
+  socklen_t len = sizeof(client_addr2);
+
+  //accept connection from client
+  client_sock=accept(sockfd,(struct sockaddr *) &client_addr2, &len );
+  client_addr->sin_addr=client_addr2.sin_addr;
+  client_addr->sin_port=client_addr2.sin_port;
+  if (client_sock == -1){
+    perror("accept:");
+  }
+
+  return client_sock;
+
+}
+
+void do_connect(int sockfd, struct sockaddr_in sock_host, int size_host){
+	if (connect(sockfd, (struct sockaddr *) & sock_host, size_host) == -1)
+			perror("error connect dmsexec");
+}
+
+ssize_t send_line(int fd, void *buf, size_t len){
+
+  /* Variables */
+  int nleft;
+  int nwritten;
+  char * ptr;
+  ptr = buf;
+  nleft = len;
+
+  /* perform the send */
+  while( nleft > 0 ){
+    if( (nwritten = write(fd, ptr, nleft)) <= 0) {
+      if(errno == EINTR){
+        nwritten = 0;
+        perror("");
+      }
+      return -1;
+    }
+    nleft -= nwritten;
+    ptr += nwritten;
+  }
+
+  return len;
+}
+
+ssize_t display_line(char * buf, int len){
+  int n=send_line(STDOUT_FILENO, buf, len);
+  printf("\n");
+
+  return n;
+
+}
+
+ssize_t read_line(int fd, char* buf, size_t len){
+
+  memset(buf,'\0', MSG_SIZE);
+
+  /* Variables */
+  int i;
+  char c;
+  int ret=1;
+  char * ptr;
+  ptr = buf;
+  int cnt = 0;
+
+  /* Perform the read */
+  for (i = 0 ; i < len; i++){
+
+    ret = read(fd, &c, 1);
+
+    if( ret == 1 && c != '\0'){
+      ptr[cnt++] = c;
+
+      if( c == '\n' && fd==STDIN_FILENO){
+        //ptr[cnt-1] = '\0';
+        return i+1;
+      }
+      if( c == '\0'){
+        return i+1;
+      }
+    }
+    else if( 0 == ret ) {
+      //ptr[cnt] = '\0';
+      break;
+    }
+  }
+  ptr[len] = '\0';
+
+  /* Empty stdin buffer in the case of too large user_input */
+  if( fd == STDIN_FILENO && i == len ){
+    char ss[10*MSG_SIZE];
+    ret = read(fd, ss, 10*MSG_SIZE);
+  }
+  return i;
+}
+
+int get_number_lign(char *txt) {
+
+  int fd;
+  fd = open(txt ,O_RDONLY);
+  int i=0,n;
+  char c;
+
+  while (1){
+    n=read(fd,&c, 1);
+    if (n==0)
+      break;
+    if (c =='\n')
+    i++;
+  }
+  close(fd);
+  return i;
+}
+
+int get_words_lign(char *txt, int number_lign, char msg[][NAME_MAX]){
+  int j=0,k=0,n,fd;
+  char c;
+  fd = open(txt ,O_RDONLY);
+
+  while (1){
+    n=read(fd,&c, 1);
+    msg[j][k]=c;
+    k++;
+    if (n==0)
+      break;
+    if (c=='\n'){
+      msg[j][k-1]='\0';
+      j++;
+      k=0;
+    }
+  }
+  close(fd);
+  return 0;
+}
+
+//Structure : /<somthg> arg1 arg2 ...| get the argn with th no_arg put at n
+char* get_argument(char source[MSG_SIZE], int no_arg){
+  char* buffer = malloc(sizeof(char)*MSG_SIZE);
+  memset(buffer,'\0', MSG_SIZE);
+  int check=0,compt, m=0;
+
+  for (compt = 0; compt < (strlen(source))*sizeof(char); compt++) {
+
+    if (source[compt] == ' ' && check != no_arg)
+      check++;
+
+    if (source[compt] == ' ' && check == no_arg){
+      buffer[m]='\0';
+      break;
+    }
+
+    if (source[compt] != ' ' && check == no_arg-1){
+      buffer[m] = source[compt];
+      m++;
+    }
+
+
+  }
+
+  return buffer;
+}
---
+++ PROJET_SYST/Phase2/dsmexec.c
@@ -0,0 +1,221 @@
+#include "common_impl.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+// dsmwrap devra etre connu de tous. On code en dur l'endroit ou il est.
+#define PATH_DSMWRAP "/net/t/vmalay/T2/PROJET_SYST/Phase2/bin/dsmwrap"
+#define PATH_TRUC "/net/t/vmalay/T2/PROJET_SYST/Phase2/bin/truc"
+
+/* variables globales */
+#define MSG_SIZE 1024
+/* un tableau gerant les infos d'identification des processus dsm */
+dsm_proc_t *proc_array = NULL;
+
+/* le nombre de processus effectivement crees */
+volatile int num_procs_creat = 0;
+
+void usage(void)
+{
+  fprintf(stdout,"Usage : dsmexec machine_file executable arg1 arg2 ...\n");
+  fflush(stdout);
+  exit(EXIT_FAILURE);
+}
+
+void sigchld_handler(int sig)
+{
+    pid_t pid;
+
+    while ((pid = waitpid(-1, NULL, WNOHANG)) != -1)
+    {
+      if (pid > 0)
+      {
+        num_procs_creat--;
+      }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 3){
+    usage();
+  } else {
+
+    pid_t pid;
+    int port_num=0;
+    int i,j,k;
+    int num_procs=0;
+
+    int fd=0;
+    char buffer[MSG_SIZE];
+    char port[10];
+    char hostname[258];
+    char rang[10];
+    int** pipefd_stderr;
+    int** pipefd_stdout;
+
+    /* Mise en place d'un traitant pour recuperer les fils zombies*/
+    struct sigaction action;
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = &sigchld_handler;
+    action.sa_flags = 0;
+    sigaction(SIGALRM, &action, NULL);
+
+    /* lecture du fichier de machines */
+
+    /* 1- on recupere le nombre de processus a lancer */
+    num_procs=get_number_lign(argv[1]);
+    
+    /* 2- on recupere les noms des machines :  */
+    char nom_procs[num_procs][NAME_MAX];
+    get_words_lign(argv[1], num_procs, nom_procs);
+
+    /* la machine est un des elements d'identification */
+
+    /* creation de la socket d'ecoute + ecoute effective */
+    fd = creer_socket(SOCK_STREAM,&port_num);
+    do_listen(fd);
+
+    /* Initialisation de pipe stderr et stdout */
+    pipefd_stderr=malloc(sizeof(int)*num_procs);
+    pipefd_stdout=malloc(sizeof(int)*num_procs);
+
+     for(k = 0; k < num_procs ; k++) {
+       pipefd_stderr[k]=malloc(2*sizeof(int));
+       pipefd_stdout[k]=malloc(2*sizeof(int));
+     }
+
+    /* creation des fils */
+    for(i = 0; i < num_procs ; i++) {
+
+      pid = fork();
+
+      if(pid == -1) ERROR_EXIT("fork");
+
+      if (pid == 0) { /* fils */
+
+         // /* redirection stdout */
+          if (dup2(pipefd_stdout[i][1],STDOUT_FILENO) == -1)
+          perror("dup2 stdout");
+
+         // /* redirection stderr */
+          if (dup2(pipefd_stderr[i][1],STDERR_FILENO) == -1)
+          perror("dup2 stderr");
+
+        /* Creation du tableau d'arguments pour le ssh */
+        char ** newargv = malloc((argc+4)*sizeof(char *));
+        gethostname(hostname,258);
+        sprintf(port,"%i",port_num);
+        sprintf(rang,"%d", i);
+        newargv[0]="ssh";
+        newargv[1]=nom_procs[i];
+        newargv[2]=PATH_DSMWRAP;
+        newargv[3]=hostname;
+        newargv[4]=port;
+        newargv[5]=rang;
+        newargv[6]=PATH_TRUC;
+        for (k = 7; k < argc + 3; k++)
+        	newargv[k]=argv[k-3];
+        newargv[argc+3] = NULL;
+
+        /* jump to new prog : */
+        fprintf(stdout,"\n*** ___EXECUTION_SSH___ ***\n");
+        execvp("ssh",newargv);
+
+      } else  if(pid > 0) { /* pere */
+        /* fermeture des extremites des tubes non utiles */
+        for(j = 0; j < num_procs ; j++){
+          close(pipefd_stdout[j][0]);
+          close(pipefd_stderr[j][0]);
+        }
+        num_procs_creat++;
+      }
+    }
+
+    struct processus_info{
+      pid_t pid_distant;
+      int port_distant;
+      int new_sockfd;
+      int size_hostname;
+      int rang;
+      char *hostname;
+    };
+
+    struct processus_info proc[num_procs];
+
+    fprintf(stdout,"\n*** ___ACCEPT_PROCESSUS___ ***\n");
+
+    sleep(2);
+
+    for(k = 0; k < num_procs ; k++){
+
+      /* on accepte les connexions des processus dsm */
+      struct sockaddr_in client_addr;
+      socklen_t len = sizeof(client_addr);
+      proc[k].new_sockfd = accept(fd,(struct sockaddr*) &client_addr, &len );
+
+
+  	  read_line(proc[k].new_sockfd, buffer, MSG_SIZE);
+
+      /*  On recupere le nom de la machine distante */
+      	/* 1- d'abord la taille de la chaine */
+      proc[k].size_hostname = atoi(get_argument(buffer,1));
+
+  	  	/* 2- puis la chaine elle-meme */
+      proc[k].hostname = get_argument(buffer,2);
+
+      /* On recupere le pid du processus distant  */
+      proc[k].pid_distant = atoi(get_argument(buffer,3));
+
+      /* On recupere le n°port de la sock ecoute des processus distants */
+      proc[k].port_distant = atoi(get_argument(buffer,4));
+
+      /* On recupere le rang des processus distants */
+      proc[k].rang = atoi(get_argument(buffer,5));
+
+      printf("proc[%d] a reçu: %d, %s, %d, %d, %d\n",k, proc[k].size_hostname,
+      proc[k].hostname, proc[k].pid_distant, proc[k].port_distant, proc[k].rang);
+
+    }
+
+    printf("*** ___ SORTIE BOUCLE1 ___ ***\n");
+
+    /* envoi du nombre de processus aux processus dsm*/
+    /* envoi des rangs aux processus dsm */
+    /* envoi des infos de connexion aux processus */
+    for(k = 0; k < num_procs ; k++){
+      memset(buffer, '\0', MSG_SIZE);
+      sprintf(buffer,"%d %d %d %s", num_procs, proc[k].rang, proc[k].port_distant, proc[k].hostname);
+      send_line(proc[k].new_sockfd, buffer, strlen(buffer)+1);
+    }
+
+    printf("SORTIE BOUCLE2\n");
+
+    /* gestion des E/S : on recupere les caracteres */
+    /* sur les tubes de redirection de stdout/stderr */
+    /* while(1)
+    {
+    je recupere les infos sur les tubes de redirection
+    jusqu'à ce qu'ils soient inactifs (ie fermes par les
+    processus dsm ecrivains de l'autre cote ...)
+  };
+  */
+
+  /* on attend les processus fils */
+    for (k = 0; k < num_procs; k++)
+      wait(NULL);
+
+  /* on ferme les descripteurs proprement */
+    for (k = 0; k < num_procs; k++)
+      close(proc[i].new_sockfd);
+
+  printf("SORTIE BOUCLE3\n");
+  /* on ferme la socket d'ecoute */
+  close(fd);
+}
+exit(EXIT_SUCCESS);
+}
